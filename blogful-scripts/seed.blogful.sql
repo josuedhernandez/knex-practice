e4dbfa6dbda2a6b8961ba075bff674231006d3c5
Josue Hernandez <moradel82@gmail.com>
2020-11-15 19:02:52 -0700

    Added blogful database scripts

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+    ('Fish tricks', 'Main fish tricks', now() - '21 days'::INTERVAL),
+    ('Not Dogs', 'Snack for babies', now() - '21 days'::INTERVAL),
+    ('Bluffalo Wings', 'Snack for adults', now() - '21 days'::INTERVAL),
+    ('SubstiTuna Salad', 'Lunch for grandma', now() - '21 days'::INTERVAL),
+    ('Tofurkey', 'Breakfast for dad', now() - '21 days'::INTERVAL),
+    ('Pretenderloins', 'Main for family', now() - '9 days'::INTERVAL),
+    ('Steak-believe', 'Main for coworkers', now() - '9 days'::INTERVAL),
+    ('Kale Seitan', 'Breakfast everyon', now() - '9 days'::INTERVAL),
+    ('NoBull Burger', 'Snack neighbors', now() - '9 days'::INTERVAL),
+    ('Turnip the Beet', 'Lunch cousins', now() - '9 days'::INTERVAL),
+    ('Mascarphony', 'Lunch aunt', now() - '7 days'::INTERVAL),
+    ('Burgatory', 'Main children', now() - '7 days'::INTERVAL),
+    ('Sleight of Ham', 'Lunch friends', now() - '5 days'::INTERVAL),
+    ('Antichovies', 'Breakfast people', now() - '5 days'::INTERVAL),
+    ('Lettuce B. Frank', 'Lunch uncle', now() - '5 days'::INTERVAL),
+    ('Pepperphony', 'Breakfast niece', now() - '5 days'::INTERVAL),
+    ('Shamburger', 'Main for other people', now() - '4 days'::INTERVAL),
+    ('Facon', 'Breakfast more friens', now() - '4 days'::INTERVAL),
+    ('Salami-get-this-straight', 'Snack new family', now() - '4 days'::INTERVAL),
+    ('Mi-steak', 'Main staff', now() - '3 days'::INTERVAL),
+    ('Ghost Beef', 'Main all the office', now() - '3 days'::INTERVAL)
+;
